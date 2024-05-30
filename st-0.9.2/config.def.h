8b0399ab3d28794c89cf2a41356387beec19715e
Abhra <abhramondal9674@gmail.com>
2024-05-30 16:55:42 +0530

    Update config.def.h

--- st-0.9.2/config.def.h
+++ st-0.9.2/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"FantasqueSansM Nerd Font:pixelsize=14:antialias=true:autohint=true",
