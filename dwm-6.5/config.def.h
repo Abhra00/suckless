2c033904166e9bb61dca301960f770fe3a395e5a
Abhra <abhramondal9674@gmail.com>
2024-07-31 10:13:30 +0000

    fixed formatting

--- dwm-6.5/config.def.h
+++ dwm-6.5/config.def.h
@@ -6,51 +6,51 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int fborderpx = 5;        /* border pixel of floating windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
+static const unsigned int borderpx  	  = 0;        /* border pixel of windows */
+static const unsigned int fborderpx 	  = 5;        /* border pixel of floating windows */
+static const unsigned int snap      	  = 32;       /* snap pixel */
+static const int swallowfloating    	  = 0;        /* 1 means swallow floating windows by default */
+static const unsigned int gappih    	  = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    	  = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    	  = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    	  = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          	  = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning  = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing  = 2;   /* systray spacing */
+static const int systraypinningfailfirst  = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        	  = 1;        /* 0 means no systray */
 static const unsigned int systrayiconsize = 30; /* systray icon size in px */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
-static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 18;       /* vertical padding for statusbar */
-static const char *fonts[]          = { "IosevkaMayukaiCodepro:size=10" ,"Symbols Nerd Font:size=14" };
-static const char dmenufont[]       = "IosevkaMayukaiCodepro:size=10";
-static const int colorfultag        = 1;        /* 0 means use SchemeSel for selected non vacant tag */
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
-static char termcol0[] = "#000000"; /* black   */
-static char termcol1[] = "#ff0000"; /* red     */
-static char termcol2[] = "#33ff00"; /* green   */
-static char termcol3[] = "#ff0099"; /* yellow  */
-static char termcol4[] = "#0066ff"; /* blue    */
-static char termcol5[] = "#cc00ff"; /* magenta */
-static char termcol6[] = "#00ffff"; /* cyan    */
-static char termcol7[] = "#d0d0d0"; /* white   */
-static char termcol8[]  = "#808080"; /* black   */
-static char termcol9[]  = "#ff0000"; /* red     */
-static char termcol10[] = "#33ff00"; /* green   */
-static char termcol11[] = "#ff0099"; /* yellow  */
-static char termcol12[] = "#0066ff"; /* blue    */
-static char termcol13[] = "#cc00ff"; /* magenta */
-static char termcol14[] = "#00ffff"; /* cyan    */
-static char termcol15[] = "#ffffff"; /* white   */
+static const int showbar            	  = 1;        /* 0 means no bar */
+static const int topbar             	  = 1;        /* 0 means bottom bar */
+static const int vertpad            	  = 10;       /* vertical padding of bar */
+static const int sidepad            	  = 10;       /* horizontal padding of bar */
+static const int horizpadbar        	  = 5;        /* horizontal padding for statusbar */
+static const int vertpadbar         	  = 18;       /* vertical padding for statusbar */
+static const char *fonts[]          	  = { "IosevkaMayukaiCodepro:size=10" ,"Symbols Nerd Font:size=14" };
+static const char dmenufont[]       	  = "IosevkaMayukaiCodepro:size=10";
+static const int colorfultag        	  = 1;        /* 0 means use SchemeSel for selected non vacant tag */
+static char normbgcolor[]           	  = "#222222";
+static char normbordercolor[]       	  = "#444444";
+static char normfgcolor[]          	  = "#bbbbbb";
+static char selfgcolor[]            	  = "#eeeeee";
+static char selbordercolor[]        	  = "#005577";
+static char selbgcolor[]            	  = "#005577";
+static char termcol0[] 			  = "#000000"; /* black   */
+static char termcol1[] 			  = "#ff0000"; /* red     */
+static char termcol2[] 			  = "#33ff00"; /* green   */
+static char termcol3[] 			  = "#ff0099"; /* yellow  */
+static char termcol4[] 			  = "#0066ff"; /* blue    */
+static char termcol5[] 			  = "#cc00ff"; /* magenta */
+static char termcol6[] 			  = "#00ffff"; /* cyan    */
+static char termcol7[] 			  = "#d0d0d0"; /* white   */
+static char termcol8[]  		  = "#808080"; /* black   */
+static char termcol9[]  		  = "#ff0000"; /* red     */
+static char termcol10[] 		  = "#33ff00"; /* green   */
+static char termcol11[] 		  = "#ff0099"; /* yellow  */
+static char termcol12[] 		  = "#0066ff"; /* blue    */
+static char termcol13[] 		  = "#cc00ff"; /* magenta */
+static char termcol14[] 		  = "#00ffff"; /* cyan    */
+static char termcol15[]  		  = "#ffffff"; /* white   */
 static char *termcolor[] = {
   termcol0,
   termcol1,
